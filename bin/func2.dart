6ec98d4479593e2e829cb4710e4140ccccd2f6c1
ermekadiev94 <ermash.adiev@gmail.com>
2021-11-11 20:57:45 +0600

    first commit

---
+++ bin/func2.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+void main() {
+//Задание 1 (Lesson 8 Homework)
+
+  // func(9);
+
+  //Задание 2 (Lesson 8 Homework)
+
+  // func2(1, 3);
+
+  //Задание 3 (Lesson 8 Homework)
+
+  // print(func3(10, 4, 6));
+
+  //Задание 4 (Lesson 8 Homework)
+
+//   print(func4([1, 3, 2, 9, 7, 8]));
+// }
+
+// func1(int a) {
+//   var c = a * a;
+//   print(c);
+// }
+
+// func2(int a, int b) {
+//   var c = a + b;
+//   print(c);
+// }
+
+// func3(int a, int b, int c) {
+//   var d = a - b;
+//   var f = c / d;
+//   return f;
+// }
+
+// func4(List a) {
+//   for (int i = 0; i < a.length; i++) {
+//     if (i == i++) {
+//       return ('da');
+//     } else {
+//       return ('net');
+//     }
+//   }
+}
